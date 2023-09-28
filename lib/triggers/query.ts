3dc14380122638d6493b763532a1a90bf1e25e7e
winklerj <robb@blendededge.com>
2023-09-28 15:40:39 -0400

    Upgrade ferryman and ferryman-extensions Upgrade to node.js 18

--- lib/triggers/query.ts
+++ lib/triggers/query.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
+import { IncomingHeaders, Snapshot, TokenData } from '@blendededge/ferryman-extensions/lib/ferryman-types';
 import { createGraphQLRequest, createQueryString } from '../graphql-requests';
 import { makeRequest } from '../http';
-import { Config, GenericObject, Message, Self } from '../types/global';
+import { Config, Message, Self } from '../types/global';
 import { wrapper } from '@blendededge/ferryman-extensions';
 
-async function processTrigger(this: Self, msg: Message, cfg: Config, snapshot: GenericObject): Promise<void> {
-    const self = wrapper(this, msg, cfg, snapshot);
+async function processTrigger(this: Self, msg: Message, cfg: Config, snapshot: Snapshot, headers: IncomingHeaders, tokenData: TokenData): Promise<void> {
+    const self = await wrapper(this, msg, cfg, snapshot, headers, tokenData);
     self.logger.debug('msg: ', JSON.stringify(msg));
     self.logger.debug('cfg: ', JSON.stringify(cfg));
     self.logger.debug('snapshot :', JSON.stringify(snapshot));
